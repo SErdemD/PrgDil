18b45949428d89dfcc98e91a8abe25a10edb3238
SErdemD <s.erdemdelice@gmail.com>
2023-12-05 21:13:08 +0300

    new codes

---
+++ aralarindaasal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+    int say1,say2;
+    printf("1. ve 2. sayiyi giriniz: ");
+    scanf("%d %d",&say1,&say2);
+    for(int kucukSay = fmin(say1,say2); kucukSay < fmax(say1,say2); kucukSay++)
+        for(int kucukSay2 = fmin(say1,say2); kucukSay2 < fmax(say1,say2); kucukSay2++){
+            int aralarindaAsal = 1;
+            for(int sayac = 2; sayac <= kucukSay; sayac++)
+                if (kucukSay % sayac == 0 && kucukSay2 % sayac == 0)
+                    aralarindaAsal = 0;
+            if(aralarindaAsal == 1)
+                printf("%d %d /",kucukSay,kucukSay2);        
+        }    
+}
