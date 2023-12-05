88df312fe83010caafc07878aa3ae50a8e721245
SErdemD <s.erdemdelice@gmail.com>
2023-12-05 22:17:38 +0300

    Update aralarindaasal.c

--- aralarindaasal.c
+++ aralarindaasal.c
@@ -5,7 +5,7 @@ void main(){
     printf("1. ve 2. sayiyi giriniz: ");
     scanf("%d %d",&say1,&say2);
     for(int kucukSay = fmin(say1,say2); kucukSay < fmax(say1,say2); kucukSay++)
-        for(int kucukSay2 = fmin(say1,say2); kucukSay2 < fmax(say1,say2); kucukSay2++){
+        for(int kucukSay2 = fmin(say1,say2); kucukSay2 < kucukSay; kucukSay2++){
             int aralarindaAsal = 1;
             for(int sayac = 2; sayac <= kucukSay; sayac++)
                 if (kucukSay % sayac == 0 && kucukSay2 % sayac == 0)
